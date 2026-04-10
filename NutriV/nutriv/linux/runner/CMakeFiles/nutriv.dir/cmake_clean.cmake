c44ce8ee7843631b042711273d2164931bcac7a2
Fernando-Viana <sfernandovianna@gmail.com>
2026-04-10 12:43:00 -0300

    correção flutter sdk path

---
+++ NutriV/nutriv/linux/runner/CMakeFiles/nutriv.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../intermediates_do_not_run/nutriv"
+  "../intermediates_do_not_run/nutriv.pdb"
+  "CMakeFiles/nutriv.dir/__/flutter/generated_plugin_registrant.cc.o"
+  "CMakeFiles/nutriv.dir/__/flutter/generated_plugin_registrant.cc.o.d"
+  "CMakeFiles/nutriv.dir/main.cc.o"
+  "CMakeFiles/nutriv.dir/main.cc.o.d"
+  "CMakeFiles/nutriv.dir/my_application.cc.o"
+  "CMakeFiles/nutriv.dir/my_application.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nutriv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NutriV/nutriv/linux/runner/CMakeFiles/nutriv.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nutriv.
+# This may be replaced when dependencies are built.
