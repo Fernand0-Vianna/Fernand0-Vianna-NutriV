184adcd0bb6d43997360be1413e31339ae34262e
Fernando S <sfernandovianna@gmail.com>
2026-04-30 20:19:57 -0300

    remoção dependencia Linux

---
+++ NutriV/nutriv/install_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /media/fernando/PROJETOS/NutriV/Fernand0-Vianna-NutriV/NutriV/nutriv
+export GIT_DIR=/media/fernando/PROJETOS/NutriV/Fernand0-Vianna-NutriV/NutriV/nutriv/.git
+export GIT_WORK_TREE=/media/fernando/PROJETOS/NutriV/Fernand0-Vianna-NutriV/NutriV/nutriv
+flutter pub get
--- NutriV/nutriv/linux/runner/CMakeFiles/nutriv.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for nutriv.
-# This may be replaced when dependencies are built.
